75d2ad343402fef231d8ebe5fe4a9c55813f869a
ClearLotus <1509506498@qq.com>
2026-05-07 14:58:22 +0800

    移除同源 iframe 的 sandbox，避免 allow-scripts + allow-same-origin 的浏览器安全警告

--- apps/web/src/app/share/[id]/page.tsx
+++ apps/web/src/app/share/[id]/page.tsx
@@ -53,7 +53,6 @@ export default function ReportSharePage({ params }: Props) {
         src={report.url}
         title={report.title}
         className="h-full w-full border-0"
-        sandbox="allow-scripts allow-same-origin"
         referrerPolicy="no-referrer"
       />
     </div>
